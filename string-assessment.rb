6e0fce7ca30cba4f34baa4f2e4d786f54d8d3619
Kane Baccigalupi <baccigalupi@gmail.com>
2014-10-31 15:42:54 -0700

    Update string-assessment.rb

--- string-assessment.rb
+++ string-assessment.rb
@@ -1,3 +1,8 @@
+# This works, but the regex looks funky ... like it is trying to hard
+# str.gsub(/e/, '3') works just as well
+# also str.gsub('e', '3') works too
+# Were you trying for something special with the brackets and quotes?
+
 str = "The quick brown fox jumped over the lazy brown dog."
 result = str.gsub(/['e']/, '3')
 print result.upcase
